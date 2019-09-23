c9b5034230856b20527b502c841423848318cd2a
Beneitez Gonzalo, Daniel (EXTERNO UTEL ATOSWORLDGR <jdoe@example.com>
2019-09-23 10:20:02 +0200

    Merge 1.0.2 - 1.0.1

--- prueba1/src/HolaMundo.java
+++ prueba1/src/HolaMundo.java
@@ -1,10 +1,6 @@
 public class HolaMundo {
 public static void main(String[] argumentos){
-<<<<<<< HEAD
-System.out.println("Hola mundo Master");
-=======
-System.out.println("Hola mundo 1.0.2");
+System.out.println("Hola mundo 1.0.1");
 System.out.println("Nuevo Branch");
->>>>>>> refs/remotes/origin/1.0.2
 }
 }
